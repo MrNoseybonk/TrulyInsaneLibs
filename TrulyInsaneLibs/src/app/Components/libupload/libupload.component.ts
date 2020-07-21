59e8d73a275806f4b373434252d802bada09f27a
Steven Wilson <speedy0097@yahoo.com>
2020-07-21 14:50:45 -0400

    The totals have been split up into each singular group.

--- TrulyInsaneLibs/src/app/Components/libupload/libupload.component.ts
+++ TrulyInsaneLibs/src/app/Components/libupload/libupload.component.ts
@@ -11,6 +11,18 @@ import { Subscription } from 'rxjs';
 export class LibuploadComponent implements OnInit {
   private libSub: Subscription;
 
+  totals: any;
+  nouns: number;
+  plurals: number;
+  verbs: number;
+  adjectives: number;
+  colors: number;
+  ings: number;
+  adverbs: number;
+  propers: number;
+  numbers: number;
+  pasts: number;
+
   public formGroup = this.fb.group({
     file: [null, Validators.required]
   });
@@ -20,6 +32,16 @@ export class LibuploadComponent implements OnInit {
   constructor(private fb: FormBuilder, private fileUploadService: FileuploadService) { }
 
   ngOnInit(): void {
+    this.nouns = 0;
+    this.plurals = 0;
+    this.verbs = 0;
+    this.adjectives = 0;
+    this.colors = 0;
+    this.ings = 0;
+    this.adverbs = 0;
+    this.propers = 0;
+    this.numbers = 0;
+    this.pasts = 0;
   }
 
   public onFileChange(event)
@@ -42,6 +64,27 @@ export class LibuploadComponent implements OnInit {
   public onSubmit(): void
   {
     this.libSub = this.fileUploadService.upload(this.fileName, this.formGroup.get('file').value).subscribe((resp) => {
-      console.log(resp); } );
+      this.totals = resp;
+      this.nouns = this.totals.nouns;
+      this.plurals = this.totals.plurals;
+      this.verbs = this.totals.verbs;
+      this.adjectives = this.totals.adjectives;
+      this.colors = this.totals.colors;
+      this.ings = this.totals.ings;
+      this.adverbs = this.totals.adverbs;
+      this.propers = this.totals.propers;
+      this.numbers = this.totals.numbers;
+      this.pasts = this.totals.pasts;
+      console.log(this.nouns);
+      console.log(this.plurals);
+      console.log(this.verbs);
+      console.log(this.adjectives);
+      console.log(this.colors);
+      console.log(this.ings);
+      console.log(this.adverbs);
+      console.log(this.propers);
+      console.log(this.numbers);
+      console.log(this.pasts);
+     } );
   }
 }
