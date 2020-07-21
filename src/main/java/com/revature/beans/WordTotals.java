709df8409212c3287a0a0ad4a2eadbb7ec31679b
Steven Wilson <speedy0097@yahoo.com>
2020-07-21 14:26:47 -0400

    Added a bunch more types of words to count and got it to count tokens near , . - : and all new lines.

--- src/main/java/com/revature/beans/WordTotals.java
+++ src/main/java/com/revature/beans/WordTotals.java
@@ -3,14 +3,84 @@ package com.revature.beans;
 public class WordTotals 
 {
 	private Integer nouns;
-	private Integer verbs;
-	private Integer adjectives;
+	private Integer plurals;
+	private Integer verbs; // present tense, non-ing verbs like run
+	private Integer adjectives; // for any and all adjectives
+	private Integer colors; // for when specifically looking for a color adjective
+	private Integer ings; // ing verbs like running
+	private Integer adverbs;
+	private Integer propers; // proper nouns like a persons name
+	private Integer numbers;
+	private Integer pasts; // past tense verbs like ran
 	
 	public WordTotals()
 	{
 		nouns = 0;
+		plurals = 0;
 		verbs = 0;
 		adjectives = 0;
+		colors = 0;
+		ings = 0;
+		adverbs = 0;
+		propers = 0;
+		numbers = 0;
+		pasts = 0;
+	}
+	
+	public Integer getPlurals() {
+		return plurals;
+	}
+
+	public void setPlurals(Integer plurals) {
+		this.plurals = plurals;
+	}
+
+	public Integer getColors() {
+		return colors;
+	}
+
+	public void setColors(Integer colors) {
+		this.colors = colors;
+	}
+
+	public Integer getIngs() {
+		return ings;
+	}
+
+	public void setIngs(Integer ings) {
+		this.ings = ings;
+	}
+
+	public Integer getAdverbs() {
+		return adverbs;
+	}
+
+	public void setAdverbs(Integer adverbs) {
+		this.adverbs = adverbs;
+	}
+
+	public Integer getPropers() {
+		return propers;
+	}
+
+	public void setPropers(Integer propers) {
+		this.propers = propers;
+	}
+
+	public Integer getNumbers() {
+		return numbers;
+	}
+
+	public void setNumbers(Integer numbers) {
+		this.numbers = numbers;
+	}
+
+	public Integer getPasts() {
+		return pasts;
+	}
+
+	public void setPasts(Integer pasts) {
+		this.pasts = pasts;
 	}
 	
 	public Integer getNouns() {
@@ -39,6 +109,8 @@ public class WordTotals
 	
 	@Override
 	public String toString() {
-		return "WordTotals [nouns=" + nouns + ", verbs=" + verbs + ", adjectives=" + adjectives + "]";
+		return "WordTotals [nouns=" + nouns + ", plurals=" + plurals + ", verbs=" + verbs + ", adjectives=" + adjectives
+				+ ", colors=" + colors + ", ings=" + ings + ", adverbs=" + adverbs + ", propers=" + propers
+				+ ", numbers=" + numbers + ", pasts=" + pasts + "]";
 	}
 }
--- src/main/java/com/revature/services/LibService.java
+++ src/main/java/com/revature/services/LibService.java
@@ -12,17 +12,27 @@ public class LibService
 	{
 		WordTotals totals = new WordTotals();
 		Integer nouns = 0;
+		Integer plurals = 0;
 		Integer verbs = 0;
 		Integer adjectives = 0;
-		String splitted[] = lib.split(" |,|\\.|-|:");
+		Integer colors = 0;
+		Integer ings = 0;
+		Integer adverbs = 0;
+		Integer propers = 0;
+		Integer numbers = 0;
+		Integer pasts = 0;
+		String splitted[] = lib.split(" |,|\\.|-|:|\\R");
 		
 		for (int i = 0; i < splitted.length; i++)
 		{
-			System.out.println(splitted[i]);
 			switch(splitted[i])
 			{
 				case "<noun>":
 					nouns++;
+					break;
+				case "<plural>":
+					System.out.println("Plural");
+					plurals++;
 					break;
 				case "<verb>":
 					verbs++;
@@ -30,14 +40,41 @@ public class LibService
 				case "<adjective>":
 					adjectives++;
 					break;
+				case "<color>":
+					colors++;
+					break;
+				case "<ing>":
+					System.out.println("Ing");
+					ings++;
+					break;
+				case "<adverb>":
+					System.out.println("Adverb");
+					adverbs++;
+					break;
+				case "<proper>":
+					propers++;
+					break;
+				case "<number>":
+					numbers++;
+					break;
+				case "<past>":
+					pasts++;
+					break;
 				default:
 					break;
 			}
 		}
 		
 		totals.setAdjectives(adjectives);
+		totals.setPlurals(plurals);
 		totals.setNouns(nouns);
 		totals.setVerbs(verbs);
+		totals.setAdverbs(adverbs);
+		totals.setColors(colors);
+		totals.setIngs(ings);
+		totals.setNumbers(numbers);
+		totals.setPasts(pasts);
+		totals.setPropers(propers);
 		
 		return totals;
 	}
