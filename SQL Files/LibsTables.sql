35fe09609974589197bd692f38efece4f7e7555f
Steven Wilson <speedy0097@yahoo.com>
2020-07-29 10:14:35 -0400

    Tweaked SQL files.

--- SQL Files/LibsTables.sql
+++ SQL Files/LibsTables.sql
@@ -1,12 +1,10 @@
 drop table person cascade constraints;
 drop table lib_template cascade constraints;
 drop table saved_lib cascade constraints;
-drop table person_saved cascade constraints;
 
 drop sequence person_seq;
 drop sequence template_seq;
 drop sequence saved_seq;
-drop sequence person_saved_seq;
 
 create table person
 (
@@ -26,19 +24,11 @@ create table saved_lib
 (
   id number(10) primary key,
   saved_name varchar2(20) not null,
-  lib clob not null
-);
-
-create table person_saved
-(
-  id number(10) primary key,
+  lib clob not null,
   person_id number(10) not null,
-  saved_id number(10) not null,
-  constraint fk_person_saved foreign key (person_id) references person(id),
-  constraint fk_saved_person foreign key (saved_id) references saved_lib(id)
+  constraint fk_person_saved foreign key (person_id) references person(id)
 );
 
-create sequence person_saved_seq;
 create sequence saved_seq;
 create sequence person_seq;
 create sequence template_seq;
