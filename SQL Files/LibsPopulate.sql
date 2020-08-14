342503d88df0ad09c586bedb26cb876f02897806
Steven Wilson <speedy0097@yahoo.com>
2020-08-14 09:31:27 -0400

    Updated SQL files for the updared version of the DB which has user types.

--- SQL Files/LibsPopulate.sql
+++ SQL Files/LibsPopulate.sql
@@ -1,3 +1,14 @@
+create or replace trigger user_type_pk_trig
+before insert or update on user_type
+for each row
+begin
+	if INSERTING then
+		select user_type_seq.nextval into :new.id from dual;
+	elsif UPDATING then
+		select :old.id into :new.id from dual;
+  end if;
+end;
+/
 create or replace trigger person_pk_trig
 before insert or update on person
 for each row
@@ -32,7 +43,13 @@ begin
 end;
 /
 
-insert into person (username, pssword)
-values ('swilson', 'pass');
+insert into user_type (user_type)
+values ('Admin');
+
+insert into user_type (user_type)
+values ('User');
+
+insert into person (username, pssword, user_type)
+values ('swilson', 'pass', 1);
 
 commit;
--- SQL Files/LibsTables.sql
+++ SQL Files/LibsTables.sql
@@ -1,16 +1,26 @@
+drop table user_type cascade constraints;
 drop table person cascade constraints;
 drop table lib_template cascade constraints;
 drop table saved_lib cascade constraints;
 
+drop sequence user_type_seq;
 drop sequence person_seq;
 drop sequence template_seq;
 drop sequence saved_seq;
+
+create table user_type
+(
+  id number(10) primary key,
+  user_type varchar2(10) unique not null
+);
 
 create table person
 (
   id number(10) primary key,
   username varchar2(25) unique not null,
-  pssword varchar2(25) not null
+  pssword varchar2(25) not null,
+  user_type number(10) unique not null,
+  constraint fk_person_type foreign key (user_type) references user_type(id)
 );
 
 create table lib_template
@@ -29,6 +39,7 @@ create table saved_lib
   constraint fk_person_saved foreign key (person_id) references person(id)
 );
 
+create sequence user_type_seq;
 create sequence saved_seq;
 create sequence person_seq;
 create sequence template_seq;
